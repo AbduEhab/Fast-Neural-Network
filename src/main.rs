e56c407b8410ccf1bb3046176b73bb217d184c89
Abdelrahman Ehab <abdu.ehab0@gmail.com>
2023-10-02 04:11:44 +0300

    updated example

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use fast_neural_network::{activation::*, neural_network::*};
 use ndarray::*;
 
 fn main() {
-    let mut network = Network::new(2, 1, ActivationType::Tanh, 0.005);
+    let mut network = Network::new(2, 1, ActivationType::Tanh, 0.005); // Create a new network with 2 inputs, 1 output, a tanh activation function, and a learning rate of 0.005
 
-    network.add_hidden_layer_with_size(2);
-    network.compile();
+    network.add_hidden_layer_with_size(2); // Add a hidden layer with 2 neurons
 
-    // Let's create a dataset
+    network.compile();  // Compile the network to prepare it for training
+                        // (will be done automatically during training)
+                        // The API is exposed so that the user can compile
+                        // the network on a different thread before training if they want to
+
+    // Let's create a dataset to represent the XOR function
     let mut dataset: Vec<(ndarray::Array1<f64>, ndarray::Array1<f64>)> = Vec::new();
 
     dataset.push((array!(0., 0.), array!(0.)));
@@ -15,7 +19,7 @@ fn main() {
     dataset.push((array!(0., 1.), array!(1.)));
     dataset.push((array!(1., 1.), array!(0.)));
 
-    network.train(&dataset, 50_000, 5_000);
+    network.train(&dataset, 50_000, 5_000); // train the network for 50,000 epochs with a decay_time of 5,000 epochs
 
     let mut res;
 
@@ -29,9 +33,8 @@ fn main() {
         );
     }
 
-    network.save("network.json");
+    network.save("network.json"); // Save the model as a json to a file
 
-    // let mut network = Network::load("network.json");
-
-    // println!("{:?}", network.predict(&input));
+    // Load the model from a json file using the below line
+    // let mut loaded_network = Network::load("network.json");  
 }
